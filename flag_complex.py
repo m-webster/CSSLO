c19dd65b39f88f6a15122274b291179818ebbf92
m-webster <46361145+m-webster@users.noreply.github.com>
2024-02-02 13:51:14 +0000

    Add files via upload

--- flag_complex.py
+++ flag_complex.py
@@ -204,7 +204,8 @@ def faceCycles(F,A,colours):
             CC = inducedCycles(parentTree, cycles)
         else:
             CC = FC(parentTree,cycles,F)
-        temp.update({cycle2tuple(c) for c in CC})
+            CC = {cycle2tuple(c) for c in CC}
+        temp.update(CC)
     return temp
 
 def spanningTrees(F,A):
@@ -314,7 +315,7 @@ def inducedCycles(parentTree, cycles):
         c = path2cycle(p1,p2)
         for e in eList:
             temp.append(list(c) + list(e))
-    return temp
+    return {tuple(c) for c in temp}
 
 def checkRSGNodePartial(i,c,colours,FG):
     score = 0
@@ -374,7 +375,7 @@ def doubleComplex(C):
     C[0] = np.vstack([C[0],C[0]])
     return C
 
-#####################################33
+#####################################
 
 def printAdj(A):
     for i in range(len(A)):
@@ -403,6 +404,96 @@ def flagIdentify(FG,ix):
             temp[s] = i
     return temp
 
+def flag_code_LX(FG,FI,Sxmc,n,subGraphs,lx):
+    ## number of colours
+    D = len(FG)
+    SX_types = []
+    SZ_types = []
+    ## split into Z and X-type stabilisers
+    for x,m,col in Sxmc:
+        toAdd = SX_types if x == lx else SZ_types
+        toAdd.append((m,col))
+    # print('SX_types',SX_types)
+    # print('SZ_types',SZ_types)
+    ## intialise LX
+    LX = []
+    ## iterate through each type of SZ
+    for mz, colz in SZ_types:
+        ## inverted set of colours
+        colz_inv = set(range(D)).difference(colz)
+        # print('mz, colz,colz_inv',mz, colz,colz_inv)
+        ## SX types which contain colz_inv
+        X_inc = [(mx,colx) for (mx,colx) in SX_types if colz_inv.issubset(colx)]
+        # print('X_inc',X_inc)
+        ## convert from set to tuple
+        colz_inv = cycle2tuple(colz_inv)
+        ## make chain complex
+        C0 = subGraphs[(mz,colz)] 
+        # print('C0')
+        # printAdj(C0)
+        ## by calling getSubgraphs we calculate the subgraph if necessary
+        C1 = getSubgraphs(FG,FI,subGraphs,'m',colz_inv)
+        # print('C1')
+        # printAdj(C1)
+        C2 = []
+        for (mx,colx) in X_inc:
+            C2.extend(subGraphs[(mx,colx)])
+        # print('C2')
+        # printAdj(C2)
+        ## boundary operators
+        D2 = ZMat([[1 if len(set(c1).intersection(c2)) > 0 else 0 for c1 in C1] for c2 in C2])
+        H, rowops = How(D2,2)
+        # print('D2',np.shape(D2),'rank',len(H))
+        # print(ZmatPrint(D2))
+
+        D1 = ZMat([[1 if len(set(c0).intersection(c1)) > 0 else 0 for c0 in C0] for c1 in C1])
+        K = getKer(D1.T,2)
+        # print('D1',np.shape(D1),'rank ker(D1.T)',len(K),'|L| =',len(K) - len(H))
+        # print(ZmatPrint(D1))
+        # print('D1 @ D2',np.sum(matMul(D2,D1,2)))
+        ## L = ker(D1)/Im(D2)
+        L = []
+        for x in K:
+            r, u = matResidual(H,x,2)
+            if np.sum(r) > 0:
+                L.append(x)
+        # L = indepLZ(H,K)
+        # print('L',np.shape(L))
+        # print(ZmatPrint(L))
+        ## lift back from cycles to flags and append to LX
+
+        C1 = ZMat([set2Bin(n,x) for x in C1])
+        # print('C1',np.shape(C1))
+        # print(ZmatPrint(C1))
+        LC = matMul(L,C1,2)
+        # print('LC',np.shape(LC))
+        # print(ZmatPrint(LC))
+        LX.append(LC)
+    LX = np.vstack(LX)
+    return LX
+
+def getSubgraphs(FG,FI,subGraphs,m,colours):
+    ## check if we have already calculated...
+    if (m,colours) in subGraphs:
+        return  subGraphs[(m,colours)]
+    ## if not, explicitly calculate
+    CC = rainbowSubgraphs(FG,colours) if m=="r" else maximalSubgraphs(FG,colours) 
+    ## optionally make min weight set of cycles
+    if SG:
+        CC = simplifyGens(CC)
+    ## optionally identify qubits as specified by FI
+    if FI is not None:
+        CC = mergeQubits(CC, FI)
+    ## update subGraphs for later use
+    subGraphs[(m,colours)] = CC
+    return CC
+
+def weightSort(LX):
+    '''sort rows of LX by increasing weight'''
+    w = np.sum(LX,axis=-1)
+    ix = argsort(w)
+    return LX[ix,:]
+
 def flag_code(C,Sxmc,SG=False,IdC=None):
     '''Return Flag code in binary format
     C: a list of boundary operators representing a complex
@@ -421,16 +512,10 @@ def flag_code(C,Sxmc,SG=False,IdC=None):
     SZ = []
     ## FI is a lookup table for identifying qubits in maximal subgraphs
     FI = None if IdC is None else flagIdentify(FG,IdC)
+    subGraphs = dict()
     for x,m,colours in Sxmc:
-        CC = rainbowSubgraphs(FG,colours) if m=="r" else maximalSubgraphs(FG,colours) 
-        ## optionally make min weight set of cycles
-        if SG:
-            CC = simplifyGens(CC)
-        ## optionally identify qubits as specified by FI
-        if FI is not None:
-            CC = mergeQubits(CC, FI)
-
-        print('freqTable',x,m,colours,freqTable([len(s) for s in CC]))
+        CC = getSubgraphs(FG,FI,subGraphs, m,colours)
+        print(f'subgraph type: {x},{m},{colours}; size:count {freqTable([len(s) for s in CC])}')
         if x=='x':
             SX.extend(CC)
         else:
@@ -438,7 +523,17 @@ def flag_code(C,Sxmc,SG=False,IdC=None):
     n = CC2n(SX + SZ)
     SX = ZMat([set2Bin(n, s) for s in SX])
     SZ = ZMat([set2Bin(n, s) for s in SZ])
-    return SX,SZ
+    HX, rowops = How(SX,2)
+    HZ, rowops = How(SZ,2)
+    k = n - len(HX) - len(HZ)
+    print(f'[[{n},{k}]] code')
+    LX = flag_code_LX(FG,FI,Sxmc,n,subGraphs,'x')
+    LX = indepLZ(HX,weightSort(LX))
+    print('LX weights:',freqTable(np.sum(LX,axis=-1)))
+    LZ = flag_code_LX(FG,FI,Sxmc,n,subGraphs,'z')
+    LZ = indepLZ(HZ,weightSort(LZ))
+    print('LZ weights:',freqTable(np.sum(LZ,axis=-1)))
+    return SX,SZ,LX,LZ
 
 def productComplex(HList,double=False):
     d = len(HList)
@@ -449,8 +544,7 @@ def productComplex(HList,double=False):
     C = [c.T for c in reversed(C)]
     return C if not double else doubleComplex(C)
 
-def Product_24_Cell(HList,double=False,SG=False):
-    C = productComplex(HList[:3],double)
+def Complex_to_24_Cell(C,SG=False):
     IdC = [0,3]
     xCycles = [(0,1,2),(1,2,3)]
     Sxmc = [('x','m',c) for c in xCycles]
@@ -458,8 +552,7 @@ def Product_24_Cell(HList,double=False,SG=False):
     Sxmc += [('z','r',c) for c in zCycles]
     return flag_code(C,Sxmc,SG,IdC)
 
-def Product_8_24_48_Cell(HList,double=False,SG=False):
-    C = productComplex(HList[:3],double)
+def Complex_to_8_24_48_Cell(C,SG=False):
     IdC = [3]
     xCycles = [(0,1,2),(0,2,3),(1,2,3)]
     Sxmc = [('x','m',c) for c in xCycles]
@@ -467,8 +560,7 @@ def Product_8_24_48_Cell(HList,double=False,SG=False):
     Sxmc += [('z','r',c) for c in zCycles]
     return flag_code(C,Sxmc,SG,IdC)
 
-def ProductDefault(HList,double=False,SG=False):
-    C = productComplex(HList,double)
+def Complex_to_Code(C,SG=False):
     IdC = None
     xCycles = iter.combinations(range(d+1),d)
     Sxmc = [('x','m',c) for c in xCycles]
@@ -476,8 +568,7 @@ def ProductDefault(HList,double=False,SG=False):
     Sxmc += [('z','r',c) for c in zCycles]
     return flag_code(C,Sxmc,SG,IdC)
 
-def Default2D(HList,double=False,SG=False):
-    C = productComplex(HList[:2],double)
+def Complex_to_Code_2D(C,SG=False):
     IdC = None
     rCycles = [(0,1),(1,2)]
     mCycles = [(0,2)]
@@ -485,6 +576,19 @@ def Default2D(HList,double=False,SG=False):
     Sxmc += [('z','m',c) for c in mCycles] + [('z','r',c) for c in rCycles]
     return flag_code(C,Sxmc,SG,IdC)
 
+def Complex_to_Code_test(C,SG=False):
+    IdC = None
+    D = len(C)
+    # xCycles = [(0,1,2),(1,2,3)]
+    # xCycles = [(0,1,2),(1,2,3),(0,1,3),(0,2,3)]
+    xCycles = [c for c in iter.combinations(range(D+1),D)]
+    Sxmc = [('x','m',c) for c in xCycles]
+    
+    zCycles = xCycles
+    # zCycles = [(0,1,3),(0,2,3)]
+    zCycles = [c for c in iter.combinations(range(D+1),2)]
+    Sxmc += [('z','r',c) for c in zCycles]
+    return flag_code(C,Sxmc,SG,IdC)
 
 def random_code(n_rows: int = 3, n_cols: int = 4):
     matrix = []
@@ -518,55 +622,147 @@ def make_even(H: np.ndarray) -> np.ndarray:
 
     return H
 
-	
+###################################
+## Reflection Group Complexes
+###################################
+
+def RGComplex(myrow):
+    temp = []
+    # print(myrow)
+    for myLabel in ['zEV','zFE','zCF']:
+        if myLabel in myrow: 
+            temp.append(str2ZMatdelim(myrow[myLabel]).T)
+    # print([np.shape(C) for C in temp])
+    return temp
+
+#############################
+## Check for quasi-transversality
+#############################
+
+def SXIntersections(SX,d):
+    r,n = np.shape(SX)
+    E = []
+    E.append([set(bin2Set(x)) for x in SX])
+    for c in range(d-1):
+        temp = set()
+        for i in range(len(E[0])):
+            for j in range(len(E[c])):
+                e = E[0][i].intersection(E[c][j])
+                if len(e) > 0 and len(e) < len(E[c][j]):
+                    temp.add(cycle2tuple(e))
+        E.append([set(e) for e in temp])
+    return E
+
+def QuasiTransversal(SX,LX,d):
+    ## intersections of up to d elements of SX
+    SXE = SXIntersections(SX,d)
+    # for i in range(len(SXE)):
+    #     print(freqTable([len(e) for e in SXE[i]]))
+    ## check they are even weight
+    for x in SXE[-1]:
+        if (len(x) % 2) > 0:
+            # print(x)
+            return False
+    ## intersections of up to d-1 elements of LX
+    LXE = SXIntersections(LX,d-1)
+    for s in range(0,d-1):
+        for t in range(0,d-1-s):
+            # print('s,t',s,t)
+            for x1 in SXE[s]:
+                for x2 in LXE[t]:
+                    if (len(x1.intersection(x2)) % 2) > 0:
+                        # print(s,t,x1,x2)
+                        return False
+    return True
+
+
+##########################################################
+## Reflection Group Codes
+##########################################################
+
+## 3D Tesselations
+## dodecahedra
+# myfile = "5-3-5-2-codes.txt"
+# icosahedra
+# myfile = "3-5-3-2-codes.txt"
+# # cubes
+# myfile = "5-3-4-2-codes.txt"
+# # cubes - Euclidean
+# myfile = "4-3-4-2-codes.txt"
+
+# Uncomment to use Reflection Group codes
+
+# codeList = importCodeList(myfile)
+# print('len(codeList)',len(codeList))
+# for i in range(len(codeList)):
+#     C = RGComplex(codeList[i])
+#     V,E = (np.shape(C[0]))
+#     print(f'{i}: |V|={V}; valid complex:{complexCheck(C)}')
+#     for d in range(len(C)):
+#         print(d+1,'Cells',freqTable([np.sum(x) for x in C[d].T]))
+
+# i = 3
+# C = RGComplex(codeList[i])
+
+
+#################################################
+## Product Complex
 #################################################
 
-## make toric code of dimension d and cycle length L
 d = 3
 L = 2
 H = repCode(L)
 HList = [H for i in range(d)]
 ## add random classical codes to product
 # HList[0] = make_even(random_code(3,4))
-# HList[1] = make_even(random_code(5,6))
+# HList[1] = make_even(random_code(3,4))
+
+## uncomment to make double complex
+double = True
 
 print('Classical Codes')
 for i in range(len(HList)):
     print(f'H_{i}')
     print(ZmatPrint(HList[i]))
 
+C = productComplex(HList,double)
+
+#################################################
+## Construct Flag Complex Code
+#################################################
+
+## dimension of complex = Clifford hierarchy level
+d = len(C)
+
 ## SG=True - return min weight stabilisers - takes longer
-SG = True
+SG = False
+
+SX, SZ, LX, LZ = Complex_to_Code_test(C,SG)
 
 ## default product code type
-SX, SZ = ProductDefault(HList,double=False,SG=SG)
+# SX, SZ = Complex_to_Code(C,SG)
 
-## default 2D code
-# SX, SZ = Default2D(HList,double=False,SG=True)
+# default 2D code
+# SX, SZ = Complex_to_Code_2D(C,SG)
 
-## These are all 3D tyes requiring d=3
+# # These are all 3D tyes requiring d=3
 
-## Based on 8-24-48 honeycomb
-# SX, SZ = Product_8_24_48_Cell(HList,double=False)
+# # Based on 8-24-48 honeycomb
+# SX, SZ = Complex_to_8_24_48_Cell(C,SG)
 
-## Based on 24-cell honeycomb
-# SX, SZ = Product_24_Cell(HList,double=False)
+# # Based on 24-cell honeycomb
+# SX, SZ = Complex_to_24_Cell(C,SG)
 
+#################################################
 ## basic analysis of code
-H, rowops = How(SX,2)
-## rank of SX
-print('SX Rank',len(H))
-## check if code is d-orthogonal
-print('SX tOrthogonal',tOrthogonal(SX,d))
+#################################################
 
-myCode = CSSCode(SX,SZ=SZ)
-Eq,SX,LX,SZ,LZ = myCode
-k,n = np.shape(LX) 
-## code parameters
-print(f'[[{n},{k}]] Code')
+comm = matMul(SX,SZ.T,2)
+print('Stabilisers commute',np.sum(comm) == 0)
 
-## calculate distance - can be slow
-if len(LZ) > 0:
-    LZ = minWeightLZ(SZ,LZ)
-    d = min(np.sum(LZ,axis=-1))
-    print('Z-Distance',d)
+print('QuasiTransversal',d,QuasiTransversal(SX,LX,d))
+# n,k = np.shape(LX)
+# if len(LZ) > 0:
+#     LZ = minWeightLZ(SZ,LZ)
+#     d = min(np.sum(LZ,axis=-1))
+#     print('Z-Distance',d,'gamma',codeGamma(n,k,d))
