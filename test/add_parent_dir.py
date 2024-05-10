706df92abf39488b7500c9080a725b38cfc50e4e
m-webster <46361145+m-webster@users.noreply.github.com>
2024-05-10 09:07:04 +0100

    Create add_parent_dir.py

---
+++ test/add_parent_dir.py
@@ -0,0 +1,7 @@
+## add parent directory to python path
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
